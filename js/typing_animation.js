e1a8fccde33ce140f3d50ee3e5996cc4043d401e
Liliya Kirichenko <147443073+Lily-Winner@users.noreply.github.com>
2024-09-04 15:50:46 +0300

    changes in typing text and portfolio

--- js/typing_animation.js
+++ js/typing_animation.js
@@ -1,6 +1,6 @@
 // Typing animation
 const typed = new Typed(".typing-text", {
-    strings: ["an Artist", " a Designer", "a Game Developer"],
+    strings: [ "a Designer", "a Game Developer"],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
